8071c8e288dd7d25471134713b1c14d2505d2b49
Rahul <rahulrawattrippy@gmail.com>
2023-08-28 09:42:51 +0530

    update button url

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import styles from "../styles/button.module.scss";
 
 const Button = () => {
-  const handleClick = () => {
-    window.open("/");
+  const handleButtonClick = () => {
+      const redirectURL = "https://chrome.google.com/webstore/detail/watchottparty/akbbdmilignoplkhminiaecccigmpdmo?hl=en-US";
+      window.open(redirectURL, "_blank");
   };
+  
   return (
-    <button className={styles.btn} onClick={handleClick}>
+    <button className={styles.btn} onClick={handleButtonClick}>
       Add to Chrome
     </button>
   );
