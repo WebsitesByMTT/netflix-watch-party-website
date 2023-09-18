f680aff7d3aa01967acfd4d74ead2aebd018ff49
Rahul <rahulrawattrippy@gmail.com>
2023-09-18 15:49:14 +0530

    change bg image

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Hero.module.scss";
 import Button from "./Button";
 
 const Hero = () => {
+  const [bgClass, setBgClass] = useState("");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setBgClass(styles["hero-mobile"]);
+      } else {
+        setBgClass(styles["hero-large"]);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <div className={styles.hero}>
+      <div className={`${styles.hero} ${bgClass}`}>
         <div className={styles.container}>
           <div className={styles.content}>
             <div className={styles["inner-container"]}>
